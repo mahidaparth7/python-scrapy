9948fa0708231d1f6459edb52ddfc2003b5912a3
parth.mahida <parth@upmetrics.co>
2017-08-22 18:23:08 +0530

    branch added in wikimedia

--- quotesbot/spiders/ecommerce.py
+++ quotesbot/spiders/ecommerce.py
@@ -11,12 +11,15 @@ class WifimediaSpider(scrapy.Spider):
         categories = response.css("a.subsubitemlink::attr(href)").extract()
         for c in categories:
             yield scrapy.Request(url=c, callback=self.parse_product)
-
+            #break	
     def parse_product(self, response):
         time.sleep(2) 
         for info in response.css('div.productborder'):
+            brand  = info.css('div.product-title a strong::text').extract_first()
+            product_name = info.css('div.product-title a span::text').extract_first()
             yield {
-                'product_name': info.css('div.product-title a::attr(title)').extract()[0],
+                'product_name': brand + ' '+ product_name,
                 'url': info.css('div.product-title a::attr(href)').extract()[0],
                 'price': info.css('div.product-price .price::text').extract_first()[1:]
-            }  
+            }
+            #break
